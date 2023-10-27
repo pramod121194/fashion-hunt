45353640e7bce6369483ed232db46fd8d67bb20d
pramod121194 <pramod121194@gmail.com>
2023-10-27 17:47:21 +0530

    get product from database

--- backend/server.js
+++ backend/server.js
@@ -2,35 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+
 const port = process.env.PORT || 5000;
 
-connectDB(); //connect to MongoDB
+connectDB();
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
-app.get("/api/apple", (req, res) => {
-  let appleBrand = products
-    .filter(function (product) {
-      return product.brand === "Apple";
-    })
-    .map(function (product) {
-      return product;
-    });
-  res.json(appleBrand);
-});
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
