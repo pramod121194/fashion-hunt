8a444b338b03b89a8da1c6cd18aba5d21b69d6bd
pramod121194 <pramod121194@gmail.com>
2023-12-20 13:04:08 +0530

    user routes and controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
@@ -12,12 +13,14 @@ connectDB();
 const app = express();
 
 app.use("/api/products", productRoutes);
-app.use(notFound);
-app.use(errorHandler);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
