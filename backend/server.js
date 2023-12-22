6e36939e76ff91b6777f64b0b6a019e6979a4696
pramod121194 <pramod121194@gmail.com>
2023-12-22 18:19:45 +0530

    updated jwt http only cookie

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
-import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
@@ -14,12 +15,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
 
 app.use(notFound);
